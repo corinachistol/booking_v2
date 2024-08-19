49b1ac41aed973aa4f5728fc09eae7a373c34c58
corina <chistolcorina19@gmail.com>
2024-08-19 23:25:35 +0300

    twig load & render

--- website/index.php
+++ website/index.php
@@ -1,3 +1,24 @@
 <?
     //for class autoloading,do not remove!
     require_once './vendor/autoload.php';
+
+    //Twig init +  config
+    $loader = new \Twig\Loader\FilesystemLoader('./templates');
+
+    $twig = new \Twig\Environment($loader, [
+        // 'cache' => '/path/to/compilation_cache',
+    ]);
+
+    $title = 'Booking Agency';
+    $tours =[
+        ['title'=> 'Expensive tour', 'price'=> '100Euro'],
+        ['title'=> 'Cheap tour', 'price'=> '1Euro'],
+        ['title'=> 'Optimal tour', 'price'=> '50Euro'],
+    ];
+
+    //render a simple page
+    $template = $twig->load('home.twig.html');
+    print($template->render([
+        'title' => $title,
+        'tours' => $tours
+    ]));
