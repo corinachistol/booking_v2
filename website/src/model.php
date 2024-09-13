e570a0884417938064e11d0c8448024efa62fdbd
corina <chistolcorina19@gmail.com>
2024-09-13 22:58:06 +0300

    delete() Hw3+HW4

--- website/src/model.php
+++ website/src/model.php
@@ -1,30 +1,121 @@
 <?
-//HW*2: try to apply DRY to the model functions
-    // function getAllTours() {
-    //     global $pdo;
-    //     $stmt = $pdo->query('SELECT * FROM tours');
-    //     $tours = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    //     return $tours;
-    // }
-
-    // function getAllReviews() {
-    //     global $pdo;
-    //     $stmt = $pdo->query('SELECT * FROM reviews');
-    //     $reviews = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    //     return $reviews;
-    // }
-
-     function getData($data): array {
+     function getData($data): Tour {
         global $pdo;
         $stmt = $pdo->query('SELECT * FROM ' . $data);
         $info = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        // var_dump($info);
       
-        return $info;
+        return new Tour($info['id'], $info['title'], $info['price'], $info['currency']);
+     }
+
+ 
+    // CLASS: 
+    // - OOP:
+    //     - props/methods
+    //     - class/interface/inheritance/abstraction
+    //     - namespace
+    //     - traits
+    //     - polymorphism
+    //     - encapsulation
+    //     - objects
+    
+    //     - SOLID
+    //     - dp: singleton, obserber, active record;
+    //     - ORM
+    //     - CRUD / BREAD (browse,read, edit,add, delete)
+
+
+    // tour model - Active Record
+    //HW3: 
+    //  1. add a column for price currency in tours table
+    //  2. create separate class named Money (value, currency )
+    //  3. refactor Tour class - use Money price instead of int 
+
+    // HW4*:
+    //  1. add: delete() - removes the DB record corresponding to this object;
+     
+    class Tour {
+        private int $id;
+        private string $title;
+        private Money $price;
+        public function __construct(int $id, string $title, Money $price) {
+            $this->id = $id;
+            $this->title = $title;
+            $this->__set("price", $price);
+        }
+
+        public function __set($name, $value) {
+            if($name == 'price'){
+                if(!empty($value) && $value instanceof Money){
+                    $this->price = $value;
+                }else{
+                    die(sprintf("Cannot leave %s empty", $name));
+                }
+            }
+        }
+
+        public function save() {
+            global $pdo;
+
+            $amount = $this->price->__get("price_amount");
+            $currency = $this->price->__get("price_currency");
+            //                                  v parameters
+            $sql = 'INSERT INTO tours VALUES(?, ?, ?, ?)';
+            $stmt = $pdo->prepare($sql);
+            //                                                      PDO prepared statements advantages
+            $stmt->execute([$this->id, $this->title, $amount, $currency]);
+        }
+
+        public function delete($id) {
+            global $pdo;
+            $sql = 'DELETE FROM tours WHERE id=?';
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$id]);
+        }
+
     }
 
-    //-------------------- #stmt --------------
-    // 0... 
-    // 1... 
-    // 2...  
+
+
+
+
+
+
+
+    class Money{
+        private int $price_amount;
+        private string $price_currency;
+        public function __construct( int $price_amount, string $price_currency){
+            $this->__set("price_amount",$price_amount );
+            $this->__set("price_currency", $price_currency);
+        }
+
+        public function __set($name, $value){
+            if($name == 'price_amount'){
+                if(!empty($value) && is_int($value) && $value > 0) {
+                    $this->price_amount = $value;
+                } else{
+                    die(sprintf("Cannot leave %s empty" , $name,));
+                }
+            } elseif ($name == 'price_currency') {
+                if(!empty($value) && is_string($value) && strlen($value) > 2 ){
+                    $this->price_currency = $value;
+                }else{
+                    die(sprintf("Cannot leave %s empty", $name ));
+                }
+            } else{
+               die(sprintf("Cannot leave %s empty", $name));
+            }
+        }
+
+        public function __get($name) {
+            if ($name == 'price_amount') return $this->price_amount;
+            elseif ($name == 'price_currency') return $this->price_currency;
+            else die(sprintf("Unknown property %s", $name));
+        }
+
+        // public function __tostring() {
+        //     return $this->price_amount . ' ' . $this->price_currency;
+        // }
+     }
--- website/src/view.php
+++ website/src/view.php
@@ -1,31 +1,6 @@
 <?
 
-    // function renderHome($title,$tours) {
-    //     global $twig;
-    //     $template = $twig->load('home.html.twig');
-
-      
-    //     print($template->render([
-    //         'title' => $title,
-    //         'tours' => $tours
-    //     ]));
-    // }
-
-
-
-    // function renderReviews($title,$reviews) {
-    //     global $twig;
-    //     $template = $twig->load('reviews.html.twig');
-
-      
-    //     print($template->render([
-    //         'title' => $title,
-    //         'reviews' => $reviews
-    //     ]));
-    // }
-
-
-    // DRY render
+     // DRY render
        function renderPage($title,$template_name, $data='', ){
         global $twig;
         $template = $twig->load($template_name. '.html.twig');
