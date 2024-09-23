c837c7cef5136882bf9f0ec621f819842a7c3adc
corina <chistolcorina19@gmail.com>
2024-09-23 12:32:04 +0300

    class Review added

--- website/index.php
+++ website/index.php
@@ -5,37 +5,37 @@
 
    // $deleted = Tour::delete(3);
 
-   $tour = Tour::getOne(15);
-   var_dump($tour);
+   // $tour = Tour::getOne(15);
+   // var_dump($tour);
 
 
-   // if($page === 'home') {
+   if($page === 'home') {
 
-   //    $tours = Tour::getAll();
-   //    $title = 'Our Fall Tours';
-   //    renderPage($title, 'home', $tours);
+      $tours = Tour::getAll();
+      $title = 'Our Fall Tours';
+      renderPage($title, 'home', $tours);
 
-   // } else if($page === 'reviews'){
+   } else if($page === 'reviews'){
 
-   //    $reviews = Tour::getAll();
-   //    $title = 'What people think';
-   //    renderPage($title,'reviews', $reviews );
+      $reviews = Review::getAll();
+      $title = 'What people think';
+      renderPage($title,'reviews', $reviews );
       
-   // } elseif ($page === 'test'){
-   //    $tour = new Tour(1, 'New Tour from ORM', new Money (50, 'MDL'));
-   //    $tour->save();
+   } elseif ($page === 'test'){
+      $review = new Review(4, 'Author_nam', 'Review Body' );
+      $review->save();
 
-   // } elseif ($page === 'delete'){
-   //    $tour = Tour::delete(1);
-   //    // print_r($tour);
+   } elseif ($page === 'delete'){
+      $tour = Tour::delete(1);
+      // print_r($tour);
     
       
-   //    // $tours->delete( 10);
-   // } 
-   // else{
+      // $tours->delete( 10);
+   } 
+   else{
       
-   //   renderPage("The page you are looking for was not found", '404');
-   // }
+     renderPage("The page you are looking for was not found", '404');
+   }
 
 
 
